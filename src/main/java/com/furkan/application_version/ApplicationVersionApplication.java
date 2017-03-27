a9df2924baf2a6acda69d356d1eeae0a2e6ba3d8
Furkan Yavuz <furkan.yavuz@aurea.com>
2017-03-27 22:57:06 +0300

    This Commit Includes: * Spring actuators * Remote shutdown *
    - Furkan Yavuz

--- src/main/java/com/furkan/application_version/ApplicationVersionApplication.java
+++ src/main/java/com/furkan/application_version/ApplicationVersionApplication.java
@@ -1,27 +1,12 @@
 package com.furkan.application_version;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApplicationVersionApplication {
 
-	private Logger logger = LoggerFactory.getLogger(ApplicationVersionApplication.class);
-
-	@Value(value = "${application.version}")
-	public String applicationVersion;
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationVersionApplication.class, args);
-	}
-
-	@Bean
-	public String gatherApplicationVersion() {
-		logger.info("Application version: " + applicationVersion);
-		return applicationVersion;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationVersionApplication.class, args);
+    }
 }
